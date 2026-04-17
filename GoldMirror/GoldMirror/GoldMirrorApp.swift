e853dffa6b5753c46adda710a226cc2ecdc5a926
TAKUYA MUKAIYAMA <mukaiyama.takuya.0207@gmail.com>
2026-04-17 21:41:52 +0900

    feat: add local GoldMirror files from MacBook

---
+++ GoldMirror/GoldMirror/GoldMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldMirrorApp.swift
+//  GoldMirror
+//
+//  Created by TAKUYA MUKAIYAMA on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct GoldMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
